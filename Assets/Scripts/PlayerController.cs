635a482f679372a01b78bffeaaa5bd7626293ec0
Luis Fernando <luispasquinelli@hotmail.com>
2022-10-25 20:04:17 -0300

    Player's animations implemented, Attack, Jump and Long Idle animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public float longIdleTime = 5f;
     public float speed = 2.5f;
+    public float jumpForce = 2.5f;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadious;
 
     private Rigidbody2D _rigidbody;
     private Animator _animator;
 
-    private Vector2 _moviment;
+    private float _longIdleTimer;
+    private Vector2 _movement;
     private bool _facingRight = true;
+    private bool _isGrounded;
+    private bool _isAttacking;
 
     void Awake()
     {
@@ -28,25 +37,60 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        _moviment = new Vector2(horizontalInput, 0f);
+        if (_isAttacking == false) {
+			
+			float horizontalInput = Input.GetAxisRaw("Horizontal");
+			_movement = new Vector2(horizontalInput, 0f);
 
-        if (horizontalInput < 0f && _facingRight) {
-            Flip();
-        } else if(horizontalInput > 0f && !_facingRight) {
-            Flip();
-        }
+			if (horizontalInput < 0f && _facingRight == true) {
+				Flip();
+			} else if (horizontalInput > 0f && _facingRight == false) {
+				Flip();
+			}
+		}
+
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadious, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && _isGrounded == true && _isAttacking == false) {
+			_rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		}
+
+        if (Input.GetButtonDown("Fire1") && _isGrounded == true && _isAttacking == false) {
+			_movement = Vector2.zero;
+			_rigidbody.velocity = Vector2.zero;
+			_animator.SetTrigger("Attack");
+		}
     }
 
     void FixedUpdate()
     {
-        float horizontalVelocity = _moviment.normalized.x * speed;
-        _rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y);
+        if (_isAttacking == false) {
+			float horizontalVelocity = _movement.normalized.x * speed;
+			_rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y);
+		}
     }
 
     void LateUpdate()
     {
-        _animator.SetBool("Idle", _moviment == Vector2.zero);
+        _animator.SetBool("Idle", _movement == Vector2.zero);
+        _animator.SetBool("IsGrounded", _isGrounded);
+        _animator.SetFloat("VerticalVelocity", _rigidbody.velocity.y);
+
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) {
+			_isAttacking = true;
+		} else {
+			_isAttacking = false;
+		}
+
+		if (_animator.GetCurrentAnimatorStateInfo(0).IsTag("Idle")) {
+			_longIdleTimer += Time.deltaTime;
+
+			if (_longIdleTimer >= longIdleTime) {
+				_animator.SetTrigger("LongIdle");
+			}
+		} else {
+			_longIdleTimer = 0f;
+		}
     }
 
     private void Flip()
